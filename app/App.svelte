a3c63132c86c33ca05d132106d86011d67ff8142
halfnelson <dpershouse@gmail.com>
2019-10-12 22:31:18 +1000

    Start of port

---
+++ app/App.svelte
@@ -0,0 +1,25 @@
+<page>
+    <actionBar title="Svelte Native App" />
+    <gridLayout>
+        <label class="info" horizontalAlignment="center" verticalAlignment="center">
+            <formattedString>
+                <span class="far" text="&#xf135;" />
+                <span text="{message}" />
+            </formattedString>
+        </label>
+    </gridLayout>
+</page>
+
+<script>
+    let message = "Blank Svelte Native app"
+</script>
+
+<style>
+    .far {
+        color: #3A53FF;
+    }
+
+    .info {
+        font-size: 20;
+    }
+</style>
